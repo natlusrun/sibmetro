e3fd214e71d590df565dac97ce62b567274646de
Nursultan Nurdaulet <63600101+natlusrun@users.noreply.github.com>
2020-11-03 20:36:24 +0600

    update

--- js/js.js
+++ js/js.js
@@ -2,3 +2,11 @@ window.addEventListener('scroll', function(){
 	var header = document.querySelector('header'); 
 	header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+function toggleMenu(){
+	let menuToggle = document.querySelector('.toggle');
+	menuToggle.classList.toggle('active');
+	let menu = document.querySelector('.menu');
+	menu.classList.toggle('active');
+	}
+

6d966c9b84b3b00f1bb5b7e81d25c6e9b5d70816
Nursultan Nurdaulet <63600101+natlusrun@users.noreply.github.com>
2020-11-06 23:35:25 +0600

    update

--- js/js.js
+++ js/js.js
@@ -1,5 +1,18 @@
+$('.count').each(function () {
+	$(this).prop('Counter',0).animate({
+		 Counter: $(this).text()
+	}, {
+		 duration: 3000,
+		 easing: 'swing',
+		 step: function (now) {
+			  $(this).text(Math.ceil(now));
+		 }
+	});
+});
+
+
 window.addEventListener('scroll', function(){
-	var header = document.querySelector('header'); 
+	const header = document.querySelector('header'); 
 	header.classList.toggle('sticky', window.scrollY > 0);
 });
 
